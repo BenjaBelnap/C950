0dbbf3f1f05d2c7e49998361fc1fbdf743aa7acd
Benjamin Belnap <ben.belnap21@gmail.com>
2025-05-14 17:07:46 -0600

    added package file and distance table

--- entities/package.py
+++ entities/package.py
@@ -1,7 +1,8 @@
 class Package:
-    def __init__(self, address, deadline, city, zipCode, weight, status):
+    def __init__(self, address, deadline, deliveredAtTime, city, zipCode, weight, status):
         self.address = address
         self.deadline = deadline
+        self.deliveredAtTime = deliveredAtTime
         self.city = city
         self.zipCode = zipCode
         self.weight = weight
